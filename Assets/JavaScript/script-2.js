2822da2caa983053c0dea71a8ffc829ad64ae354
mrott0397 <119753667+mrott0397@users.noreply.github.com>
2023-01-12 20:08:55 -0700

    Is this working?

--- Assets/JavaScript/script-2.js
+++ Assets/JavaScript/script-2.js
@@ -1,37 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //    ARRAY
-var lowercase = [`a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z`];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var upperCase = [`A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z`]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*"]
+const lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+const numericCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
-
+console.log(lowercaseCharacters);
+console.log(numericCharacters);
+console.log(uppercaseCharacters);
+console.log(specialCharacters);
 //Gathering info from series of prompts 
 function userPrompt() {
     var length = prompt("Please enter the length of your desired password between 8 and 128 characters");
-    if(length < 8 || length > 128){
+    if(length < 8 || length > 128) {
         alert("ERROR: Please enter a value greater than 8 and less than 128");
         return null;
 }
-var isLowercase = confirm("Do you want lowercase letters in your password?")
-var isUppercase = confirm("Do you want uppercase letters in your password?")
-var isNumeric = confirm("Do you want numbers in your password?")
-var isSpecial = confirm("Do you want special character's in your password?")
-if (isLowercase === false && isUppercase === false && isNumeric === false && isSpecial === false){
+let addLength = prompt;
+
+const addLowerCase = confirm("Do you want lowercase characters  in your password?")
+const addUpperCase = confirm("Do you want uppercase characters in your password?")
+const addNumeric = confirm("Do you want numbers in your password?")
+const addSpecial = confirm("Do you want special character's in your password?")
+
+let password = "";
+
+if (addLowerCase === false && addUpperCase === false && addNumeric === false && addSpecial === false) {
     alert("Must choose at least one character type");
     return null;
 }
-var userOptions = {
-    length: length,
-    isLowercase: isLowercase,
-    isUppercase: isUppercase,
-    isNumeric: isNumeric,
-    isSpecial: isSpecial
 
+if (addLowerCase) {
+    for (let index = 0; index < lowercaseCharacters.length; index++) {
+        console.log(lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)]);
+        
+    }
+    
 }
-return null
+
+if (addUpperCase) {
+    for (let index = 0; index < uppercaseCharacters.length; index++) {
+        console.log(uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)]);
+        
+    }
+    
 }
+
+
+// var userOptions = {
+//     length: length,
+//     isLowercase: isLowerCase,
+//     isUppercase: isUpperCase,
+//     isNumeric: isNumeric,
+//     isSpecial: isSpecial
+
+// }
+return null;
+}
+
 //Function: take in user info from prompts of another function and generate password
 function generatePassword() {
     var userData = userPrompt();
@@ -41,7 +68,7 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  console.log("WP is working");
+  
 //   var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
@@ -50,4 +77,4 @@ function writePassword() {
 
   
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,107 +1,107 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() 
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-
-// PSEUDOCODE (SUPER IMPORTANT)
-
-// ARRAY
-  // var lowercase = [`a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z`];
-  // var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // var upperCase = [`A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z`]
-  // var special = ["!", "@", "#", "$", "%", "^", "&", "*"]
-
-  // var results = []
-  const passwordLength - prompt("Please enter the lenth of your desired password between 8 and 128 characters");
-  if(passwordLength < 8 || passwordLength > 128){
-    alert("ERROR: Please enter a value greater than 8 and less than 128");
-
-  // }
-  // const keys = {
-  //   lowercase: "abcdefghijklmnopqrstuv",
-  //   upperCase: "ABCDEFGHIJKLMNOP",
-  //   numbers: "0123456789",
-  //   special: "!@#$%^&*"
-
-  // }
+//   passwordText.value = password;
 
 
-// USER IMPUT
-  // How many characters would you like your password to contain?
+// // PSEUDOCODE (SUPER IMPORTANT)
 
-  // Lowercase confirm
+// // ARRAY
+//   // var lowercase = [`a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z`];
+//   // var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   // var upperCase = [`A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z`]
+//   // var special = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+//   // var results = []
+//   const passwordLength - prompt("Please enter the lenth of your desired password between 8 and 128 characters");
+//   if(passwordLength < 8 || passwordLength > 128){
+//     alert("ERROR: Please enter a value greater than 8 and less than 128");
+
+//   // }
+//   // const keys = {
+//   //   lowercase: "abcdefghijklmnopqrstuv",
+//   //   upperCase: "ABCDEFGHIJKLMNOP",
+//   //   numbers: "0123456789",
+//   //   special: "!@#$%^&*"
+
+//   // }
 
 
-  if (confirm("Do you want lowercase letters in your password?" == true)) {
-    lowercase = "You pressed OK!";
-  } else {
-    lowercase = "You canceled!";
-  }
+// // USER IMPUT
+//   // How many characters would you like your password to contain?
 
-  // numbers confirm
+//   // Lowercase confirm
 
- 
-  if (confirm("Do you want numbers in your password?" == true)) {
-    lowercase = "You pressed OK!";
-  } else {
-    lowercase = "You canceled!";
-  }
 
-  // upercase confrim
+//   if (confirm("Do you want lowercase letters in your password?" == true)) {
+//     lowercase = "You pressed OK!";
+//   } else {
+//     lowercase = "You canceled!";
+//   }
+
+//   // numbers confirm
 
  
-  if (confirm("Do you want uppercase letters in your password?" == true)) {
-    lowercase = "You pressed OK!";
-  } else {
-    lowercase = "You canceled!";
-  }
-  // special character confirm 
+//   if (confirm("Do you want numbers in your password?" == true)) {
+//     lowercase = "You pressed OK!";
+//   } else {
+//     lowercase = "You canceled!";
+//   }
+
+//   // upercase confrim
+
+ 
+//   if (confirm("Do you want uppercase letters in your password?" == true)) {
+//     lowercase = "You pressed OK!";
+//   } else {
+//     lowercase = "You canceled!";
+//   }
+//   // special character confirm 
   
-  let special;
-  if (confirm("Do you want special characters in your password?" == true)) {
-    lowercase = "You pressed OK!";
-  } else {
-    lowercase = "You canceled!";
-  }
+//   let special;
+//   if (confirm("Do you want special characters in your password?" == true)) {
+//     lowercase = "You pressed OK!";
+//   } else {
+//     lowercase = "You canceled!";
+//   }
 
-  const getKeys = [
-    function lowercase() {
-      return keys.lowercase[Math.floor(math.random() * keys.lowercase.length)];
-    },
-    function upperCase() {
-      return keys.upperCase[Math.floor(math.random() * keys.upperCase.length)];
-    },
-    function numeric() {
-      return keys.numbers[Math.floor(math.random() * keys.numbers.length)];
-    },
-    function special() {
-      return keys.special[Math.floor(math.random() * keys.special.length)];
-    },
-  ]
+//   const getKeys = [
+//     function lowercase() {
+//       return keys.lowercase[Math.floor(math.random() * keys.lowercase.length)];
+//     },
+//     function upperCase() {
+//       return keys.upperCase[Math.floor(math.random() * keys.upperCase.length)];
+//     },
+//     function numeric() {
+//       return keys.numbers[Math.floor(math.random() * keys.numbers.length)];
+//     },
+//     function special() {
+//       return keys.special[Math.floor(math.random() * keys.special.length)];
+//     },
+//   ]
 
-// CONDITIONAL
-  // if the users chooses X numbers of characters total
-    // for loop that itterates X number of times
+// // CONDITIONAL
+//   // if the users chooses X numbers of characters total
+//     // for loop that itterates X number of times
 
-  // if the user chooses to add lowercase characters
-    // Randomly select Y number of characters, push it to the Result array
-  // if the user chooses to add upercase chars
-    //
-  // if the users choose to add numbers
-    //
-  // if the users choose to add special characters
-    //
+//   // if the user chooses to add lowercase characters
+//     // Randomly select Y number of characters, push it to the Result array
+//   // if the user chooses to add upercase chars
+//     //
+//   // if the users choose to add numbers
+//     //
+//   // if the users choose to add special characters
+//     //
 
-// results = ["a", "b", "c", "d",]
+// // results = ["a", "b", "c", "d",]
 
-// Display results on to the page 
-  //target text area, display results
+// // Display results on to the page 
+//   //target text area, display results
   
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
